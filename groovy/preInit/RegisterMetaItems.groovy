e6ef359a6a0052a3545c4fa276843d0e703b676d
Lemon_Elk <crosby.w.johnson@gmail.com>
2025-09-13 14:03:15 -0700

    Added Parachutes
    Added Drogue Parachutes, Main Parachutes for thick atmosphere, and Supersonic Disk Gap Band Parachutes for thin atmospheres, if you want to change the textures, they aren't very good.

--- groovy/preInit/RegisterMetaItems.groovy
+++ groovy/preInit/RegisterMetaItems.groovy
@@ -388,5 +388,10 @@ eventManager.listen {
         customMetaItems.addItem(10101, "wing_panel.fiber_reinforced_epoxy")
         customMetaItems.addItem(10102, "wing.small")
 
+        // Parachutes 10200-10300
+        customMetaItems.addItem(10200, "parachute.drogue")
+        customMetaItems.addItem(10201, "parachute.main")
+        customMetaItems.addItem(10202, "parachute.supersonic_main")
+
         log.infoMC("Finished adding metaitems")
 }
