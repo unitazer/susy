71d3c52c70e63cd52c73e5f881dc15b7e58e902b
Rick <157808400+Rick8484@users.noreply.github.com>
2025-10-22 00:28:00 +0200

    GUH

--- groovy/material/OrganicChemistryMaterials.groovy
+++ groovy/material/OrganicChemistryMaterials.groovy
@@ -3046,13 +3046,11 @@ class OrganicChemistryMaterials {
                 .color(0x5c8fb5)
                 .build()
 
-        FluorosulfonylDifluoroacetylFluorideMix = new Material.Builder(15480, SuSyUtility.susyId('fluorosulfonyl_difluoroacetyl_fluoride_mix'))
+        FluorosulfonylDifluoroacetylFluoride = new Material.Builder(15480, SuSyUtility.susyId('fluorosulfonyl_difluoroacetyl_fluoride'))
                 .liquid()
-                .components(Carbon * 2, Fluorine * 8, Oxygen * 3, Sulfur, Sodium * 2)
+                .components(Carbon * 2, Fluorine * 4, Oxygen * 3, Sulfur)
                 .color(0x37877f)
                 .build()
-
-        FluorosulfonylDifluoroacetylFluorideMix.setFormula('(C2F4O3S)(NaF2)2', true)
 
         PerfluorovinylSulfonylEther = new Material.Builder(15481, SuSyUtility.susyId('perfluorovinyl_sulfonyl_ether'))
                 .liquid()
