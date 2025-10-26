3c2db2e7e957f4a2f39bcf9e3165cf29a4bc3d99
Fiery36 <coolpunnyskeletondunker@gmail.com>
2025-10-26 00:09:21 -0400

    better melting duration

--- groovy/postInit/metallurgy/AluminiumAlloys.groovy
+++ groovy/postInit/metallurgy/AluminiumAlloys.groovy
@@ -10,7 +10,7 @@ INDUCTION_FURNACE.recipeBuilder()
         .inputs(ore('dustTinyChrome') * 1)
         .fluidOutputs(fluid('aluminium_alloy_6061') * 10368)
         .EUt(VA[LV])
-        .duration(90)
+        .duration(9600)
         .buildAndRegister()
 
 // 90.6% Al 5.5% Zn 2.4% Mg 1.3% Cu 0.26% Cr
@@ -22,7 +22,7 @@ INDUCTION_FURNACE.recipeBuilder()
         .inputs(ore('dustTinyChrome') * 1)
         .fluidOutputs(fluid('aluminium_alloy_7075') * 11520)
         .EUt(VA[LV])
-        .duration(90)
+        .duration(9600)
         .buildAndRegister()
 
 
