1baf2d10381c3660d82c1829e9d8ca156f4522de
Rick <157808400+Rick8484@users.noreply.github.com>
2025-10-28 17:13:25 +0100

    Add 2nd recipe for turret ammo for napalm

--- groovy/postInit/mod/OpenModularTurrets.groovy
+++ groovy/postInit/mod/OpenModularTurrets.groovy
@@ -295,6 +295,14 @@ for (solder in soldering_alloys) {
 
 MIXER.recipeBuilder()
         .inputs(ore('dustClay'))
+        .fluidInputs(liquid('gasoline') * 1000)
+        .outputs(item('openmodularturrets:ammo_meta:0')*32)
+        .duration(20)
+        .EUt(VA[LV])
+        .buildAndRegister();
+
+MIXER.recipeBuilder()
+        .inputs(ore('dustClay'))
         .fluidInputs(liquid('napalm') * 1000)
         .outputs(item('openmodularturrets:ammo_meta:0')*32)
         .duration(20)
